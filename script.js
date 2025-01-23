8f6949921f29f9b2a2212463ab5765be60679292
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-01-23 20:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function getFormvalue() {
-  // Get the form elements
-  const firstNameInput = document.getElementById("firstName");
-  const lastNameInput = document.getElementById("lastName");
+      const firstNameInput = document.getElementById("firstName");
+      const lastNameInput = document.getElementById("lastName");
 
-  // Get the entered values
-  const firstName = firstNameInput.value;
-  const lastName = lastNameInput.value;
+      const firstName = firstNameInput.value;
+      const lastName = lastNameInput.value;
 
-  // Concatenate the full name
-  const fullName = firstName + " " + lastName;
+      const fullName = firstName + " " + lastName;
 
-  // Display the full name in an alert
-  alert(fullName);
-}
+      alert(fullName);
+    }
