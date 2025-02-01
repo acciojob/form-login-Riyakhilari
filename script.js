bd8ec597957693f3816ac6011a4dd4a4ebd98172
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-01 20:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-describe('Form Submission', () => {
-  it('should allow a user to input their first and last name and submit the form', () => {
-    cy.visit('/'); 
-
-    cy.get('#firstName').type('John');
-    cy.get('#lastName').type('Doe');
-    cy.get('button[type="submit"]').click();
-
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal('John Doe');
-    });
-  });
-
-  it('should allow a user to input their first, middle and last name and submit the form', () => {
-    cy.visit('/'); 
-
-    cy.get('#firstName').type('John');
-    cy.get('#lastName').type('Mark Doe'); 
-    cy.get('button[type="submit"]').click();
-
-    cy.on('window:alert', (text) => {
-      expect(text).to.equal('John Mark Doe');
-    });
-  });
-});
+function getFormvalue() {
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    alert(firstName + " " + lastName);
+}
